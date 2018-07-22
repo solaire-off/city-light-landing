ff223259f4ed7f1d59ad31bc3bc3e9b8e647c384
solaire-off <victor374288@mail.ru>
2018-07-22 16:00:02 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,31 @@
+$(function() {
+
+  $("#block-preview").bgswitcher({
+    images: [
+             // "/img/preview-default.png",
+             "/img/preview-red.png",
+             "/img/preview-all.png",
+             "/img/preview-orange.png",
+             "/img/preview-all.png",
+            ],
+
+            interval: 6500,
+            effect: 'clip',
+            duration: 5500,
+  });
+
+  $( window ).resize(function() {
+
+    if ($( window ).width() < 900){
+        $("#block-preview").bgswitcher({
+          effect: 'fade',
+        })
+      }
+    else{
+        $("#block-preview").bgswitcher({
+          effect: 'clip',
+        })
+    }
+  });
+
+});
